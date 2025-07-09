b2a0c298f1cecfb903bc979788754bb5c17cc57b
mariabiriucova <maria.biriucova@codefactorygroup.com>
2025-07-09 14:17:00 +0300

    Initial commit

---
+++ src/main/java/com/example/anagramfinderinternshipglobaldatabase/AnagramFinderInternshipGlobaldatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.example.anagramfinderinternshipglobaldatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnagramFinderInternshipGlobaldatabaseApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AnagramFinderInternshipGlobaldatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/anagramfinderinternshipglobaldatabase/AnagramFinderInternshipGlobaldatabaseApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.anagramfinderinternshipglobaldatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnagramFinderInternshipGlobaldatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
